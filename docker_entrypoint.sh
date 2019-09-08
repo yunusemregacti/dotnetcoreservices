10aa4d3b1044fde6a8cf4dde9d4611d0bc28f149
Yunus Emre Gaçti <yunusemregacti@hotmail.com>
2019-09-08 17:01:48 +0300

    Update docker_entrypoint.sh

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd /pipeline/source/app/publish
-dotnet StatlerWaldorfCorp.TeamService.dll
+echo "starting"
+mkdir /pipeline/source/app/publish/tmp
+# export TEMP=/pipeline/source/app/publish/tmp
+export TMPDIR=/pipeline/source/app/publish/tmp
+dotnet StatlerWaldorfCorp.TeamService.dll --server.urls=http://0.0.0.0:${PORT-"8080"}
