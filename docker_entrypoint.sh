56cf9e304070e0ceb031e903b58f711d7c65d174
Yunus Emre Gaçti <yunusemregacti@hotmail.com>
2019-09-08 17:14:12 +0300

    created a new entrypoint

--- docker_entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /pipeline/source/app/publish
-echo "starting"
-mkdir /pipeline/source/app/publish/tmp
-# export TEMP=/pipeline/source/app/publish/tmp
-export TMPDIR=/pipeline/source/app/publish/tmp
-dotnet StatlerWaldorfCorp.TeamService.dll --server.urls=http://0.0.0.0:${PORT-"8080"}
